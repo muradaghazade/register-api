1a79955945ed590b4c217c4c8d512d5cc6d0198f
Abdiev003 <aliabdiyev000@gmail.com>
2021-02-13 22:17:18 +0400

    Fixed image url

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='images/')
     email = models.EmailField(('email adress'), unique=True, null=True)
     password2 = models.CharField(('password2'), max_length=200)
 
